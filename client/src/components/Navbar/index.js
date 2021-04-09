11c68e5f45402fc7a524ebb0de716abdbfec822a
Mike Sasiela <m5455y@gmail.com>
2021-04-08 19:30:33 -0500

    updated color scheme and functionality of navbar component

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faBars } from "@fortawesome/free-solid-svg-icons";
 
 function NavTabs() {
   const location = useLocation();
 
+  const [menuButton, setMenuButton] = useState({
+    icon: faBars,
+  });
+
+  const handleClick = () => {
+    if (menuButton.icon === faBars) {
+      return setMenuButton({ icon: faTimes });
+    } else {
+      return setMenuButton({ icon: faBars });
+    }
+  };
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" expand="lg">
       <Navbar.Brand as={Link} to="/">
-        <img src="imgs/brewcrew.png" alt="brand icon" width="75" height="75" />{" "}
+        <img src="imgs/brewcrew.png" alt="brand icon" width="90" height="90" />{" "}
       </Navbar.Brand>
 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle
+        aria-controls="basic-navbar-nav"
+        style={{ border: "none" }}
+        onClick={() => handleClick()}
+      >
+        <span>
+          <FontAwesomeIcon
+            className="fa"
+            icon={menuButton.icon}
+            size="2x"
+            color="#d4af49"
+          />
+        </span>
+      </Navbar.Toggle>
+
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link
             as={Link}
             to="/"
             className={
-              location.pathname === "/" ? "nav-link active" : "nav-link"
+              location.pathname === "/"
+                ? "nav-link active mx-auto"
+                : "nav-link mx-auto"
             }
           >
             Home
@@ -28,8 +58,8 @@ function NavTabs() {
             to="/favorites"
             className={
               location.pathname === "/favorites"
-                ? "nav-link active"
-                : "nav-link"
+                ? "nav-link active mx-auto"
+                : "nav-link mx-auto"
             }
           >
             Favorites
@@ -38,7 +68,9 @@ function NavTabs() {
             as={Link}
             to="/signup"
             className={
-              location.pathname === "/signup" ? "nav-link active" : "nav-link"
+              location.pathname === "/signup"
+                ? "nav-link active mx-auto"
+                : "nav-link mx-auto"
             }
           >
             Login/Signup
