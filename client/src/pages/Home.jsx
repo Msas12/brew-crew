8f3c9cd67f6290c3428198d482dedfd9e3186c0c
anniemcfarland7 <anniemcfarland@aol.com>
2021-04-17 13:24:29 -0500

    working cards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
-import Brewery from '../components/Brewery'
+import Brewery from "../components/Brewery";
+import { useUserContext } from "../utils/GlobalState";
 
 function Home() {
+  const [state, dispatch] = useUserContext();
   return (
-<main>
-  <SearchBar setBreweries={setBrewieres}/>
-  <Brewery breweries={breweries}/>
-
-</main>
-  )
+    <main>
+      <SearchBar />
+      {!state.breweries
+        ? ""
+        : state.breweries.map((brewery) => {
+            return <Brewery brewery={brewery} />;
+          })}
+    </main>
+  );
 }
 
 export default Home;
