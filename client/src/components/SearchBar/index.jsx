303de3ac74ebc9db721754b3235f341170256a3b
Sportsguy <mahoward2s@gmail.com>
2021-04-17 11:03:47 -0500

    search bar fix, console log in api

--- client/src/components/SearchBar/index.jsx
+++ client/src/components/SearchBar/index.jsx
@@ -1,7 +1,8 @@
 import React, { useRef } from "react";
-import { Form, Button } from "react-bootstrap";
+import { InputGroup, Button, FormControl } from "react-bootstrap";
 import searchBrewery from '../../utils/API'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function SearchBar() {
 
@@ -14,27 +15,29 @@ function SearchBar() {
     } else {
       const newSearch = (searchText.current.value)
       searchBrewery(newSearch)
+    }
   }
-}
 
-return (
-  <Form className="searchForm">
-    <Form.Group controlId="formBasicEmail">
-      <Form.Label>Brewery Search</Form.Label>
-      <Form.Control type="text" ref={searchText} placeholder="Enter a City or State" />
-      <Button
-        className="searchBtn"
-        variant="primary"
-        type="submit"
-        onClick={handleSubmit}
-        style={{ backgroundColor: "#d4af49", border: "none" }}
-      >
-        Search
-      </Button>
-    </Form.Group>
-
-  </Form>
-);
+  return (
+    <InputGroup className="mt-5 mb-3 searchForm">
+      <FormControl
+        placeholder="Enter a City or State"
+        aria-label="Enter a City or State"
+        aria-describedby="Brewery Search"
+      />
+      <InputGroup.Append>
+        <Button
+          className="searchBtn"
+          variant="primary"
+          type="submit"
+          onClick={handleSubmit}
+          style={{ backgroundColor: "#d4af49", border: "none" }}
+        >
+          <FontAwesomeIcon icon={faSearch} />
+        </Button>
+      </InputGroup.Append>
+    </InputGroup>
+  );
 }
 
 export default SearchBar;
